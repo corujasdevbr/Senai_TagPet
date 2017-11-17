507cc249849e37fe51595b9cafbe9822518e30dd
Fernando Henrique <fernando.guerra@corujasdev.com.br>
2017-11-17 15:47:35 -0200

    Estrutura Projeto

---
+++ Senai.TagPet.ProjetoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senai.TagPet.ProjetoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
